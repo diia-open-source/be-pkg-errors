8c7996966235ca23c31b5f2803bac3cc3931066b
DIIA <opensource@thedigital.gov.ua>
2026-08-26 21:36:18 +0000

    Releasing pkg-errors

--- src/interfaces/errorType.ts
+++ src/interfaces/errorType.ts
@@ -1,7 +1,7 @@
 /* Set ErrorType according to an error use case:
  * Operated: used when an error occurred on an absolutely valid path. For example, when a resource expires on a user request, when a resource is not found, which is a valid case, etc. These errors don't count for alerts.
  *
- * External: used for errors caused by external registries or systems, so the internal team can't fix it alone.
+ * External: used for errors like an outage or timeouts caused by external registries or systems, so the internal team can't fix it alone. "Not Found" errors for resources are usually not External errors.
  *
  * Additionally, if these errors (both operated and external) are sent to a mobile client, it should include a process code to display a clear and concise issue description to the user.
  *
